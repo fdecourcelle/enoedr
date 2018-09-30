9c3a10f2ae927a5c1468bf714d7660c473d6bdfd
Francois Decourcelle <frdecourcelle@gmail.com>
2018-09-30 10:51:07 +0200

    Ionic angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
+import { HttpModule } from "@angular/http";
 
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
@@ -10,16 +11,26 @@ import { ContactPage } from "../pages/contact/contact";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
+import { DishProvider } from "../providers/dish/dish";
+import { LeaderProvider } from "../providers/leader/leader";
+import { PromotionProvider } from "../providers/promotion/promotion";
+import { ProcessHttpmsgProvider } from "../providers/process-httpmsg/process-httpmsg";
+import { baseURL } from "../shared/baseurl";
 
 @NgModule({
   declarations: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
-  imports: [BrowserModule, IonicModule.forRoot(MyApp)],
+  imports: [BrowserModule, HttpModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
   entryComponents: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: ErrorHandler, useClass: IonicErrorHandler }
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    DishProvider,
+    LeaderProvider,
+    PromotionProvider,
+    ProcessHttpmsgProvider,
+    { provide: "BaseURL", useValue: baseURL }
   ]
 })
 export class AppModule {}
