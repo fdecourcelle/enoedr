f64c157843da8be68a1c61d050f45fd3cc3092f5
Francois Decourcelle <frdecourcelle@gmail.com>
2018-09-30 15:48:43 +0200

    ionic navigation

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { ErrorHandler, NgModule } from "@angular/core";
 import { IonicApp, IonicErrorHandler, IonicModule } from "ionic-angular";
 import { HttpModule } from "@angular/http";
 
+import { baseURL } from "../shared/baseurl";
 import { MyApp } from "./app.component";
 import { HomePage } from "../pages/home/home";
 import { AboutPage } from "../pages/about/about";
 import { MenuPage } from "../pages/menu/menu";
 import { ContactPage } from "../pages/contact/contact";
+import { DishdetailPage } from "../pages/dishdetail/dishdetail";
 
 import { StatusBar } from "@ionic-native/status-bar";
 import { SplashScreen } from "@ionic-native/splash-screen";
@@ -15,13 +17,26 @@ import { DishProvider } from "../providers/dish/dish";
 import { LeaderProvider } from "../providers/leader/leader";
 import { PromotionProvider } from "../providers/promotion/promotion";
 import { ProcessHttpmsgProvider } from "../providers/process-httpmsg/process-httpmsg";
-import { baseURL } from "../shared/baseurl";
 
 @NgModule({
-  declarations: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
+  declarations: [
+    MyApp,
+    HomePage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
+    DishdetailPage
+  ],
   imports: [BrowserModule, HttpModule, IonicModule.forRoot(MyApp)],
   bootstrap: [IonicApp],
-  entryComponents: [MyApp, HomePage, AboutPage, MenuPage, ContactPage],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    AboutPage,
+    MenuPage,
+    ContactPage,
+    DishdetailPage
+  ],
   providers: [
     StatusBar,
     SplashScreen,
